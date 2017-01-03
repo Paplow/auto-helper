c081d0ecfdcdf783c621ef1076ea02522f631fff
Paplow <paplow01@gmail.com>
2017-01-03 02:38:58 +0100

    Configuration file getting its modifications as well.

--- src/config/auto-helper.php
+++ src/config/auto-helper.php
@@ -2,9 +2,12 @@
 
 	return [
 
-		// Specify path to your helper directory
-		// By default it will be "app/Helpers"
-		// meaning it'll reside in your app directory
+		/*
+		* Specify path to your helper directory
+		* By default it will be "app/Helpers"
+		* meaning it'll reside in your app directory
+		 */
+
 		'helper_path'	=>	function_exists('env') ? env('HELPER_PATH', 'app/Helpers') : ''
 
 	];
